d4096baa2430b30fdc4dd0cbff9ec96a7fd1c636
Yannick <ymenager@users.noreply.github.com>
2019-07-04 13:47:25 -0700

    Support multiple npm repos

--- prepare-build.sh
+++ prepare-build.sh
@@ -12,13 +12,26 @@ if [[ -n "${PGP_KEY}" ]]; then
      gpg --batch --import ${TMPFILE}
  fi
 fi
-if [[ -n "${NPM_USER}" ]]; then
-    NPMPARAMS="-u ${NPM_USER} -p ${NPM_PASS} -e ${NPM_EMAIL}"
-    NPM_TOKEN=$(curl -s -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"name\": \"${NPM_USER}\", \"password\": \"${NPM_PASS}\"}" ${NPM_REGISTRY}-/user/org.couchdb.user:${NPM_USER} 2>&1 | jq -r .token )
-    if [[ -n "${NPM_SCOPE}" ]]; then
-        REG=$(echo "${NPM_REGISTRY}" | sed "s/^http:/:/" | sed "s/^https://")
-        echo >>~/.npmrc "${NPM_SCOPE}:registry=${NPM_REGISTRY}";
-    fi
-    echo >>~/.npmrc "${REG}:_authToken=${NPM_TOKEN}"
-fi
 
+npmLogin() {
+    local user=$1
+    local pw=$2
+    local email=$3
+    local token=$4
+    local registry=$5
+    local scope=$6
+    if [[ -n "${user}" ]]; then
+        local token=$(curl -s -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"name\": \"${user}\", \"password\": \"${pw}\"}" ${registry}-/user/org.couchdb.user:${user} 2>&1 | jq -r .token )
+    fi
+    if [[ -n "${token}" ]]; then
+        local reg=$(echo "${registry}" | sed "s/^http:/:/" | sed "s/^https://")
+        if [[ -n "${scope}" ]]; then
+            npm config set "${scope}:registry" ${registry}
+        fi
+        npm config set "${reg}/:_authToken" ${token}
+    fi
+}
+
+npmLogin "${NPM_USER}" "${NPM_PASS}" "${NPM_EMAIL}" "${NPM_TOKEN}" "${NPM_REGISTRY}" "${NPM_SCOPE}"
+npmLogin "${NPM_USER2}" "${NPM_PASS2}" "${NPM_EMAIL2}" "${NPM_TOKEN2}" "${NPM_REGISTRY2}" "${NPM_SCOPE2}"
+npmLogin "${NPM_USER3}" "${NPM_PASS3}" "${NPM_EMAIL3}" "${NPM_TOKEN3}" "${NPM_REGISTRY3}" "${NPM_SCOPE3}"
