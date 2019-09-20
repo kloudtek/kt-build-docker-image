837409a0d113ddbb9834c5ba565f6ed61ee02310
Yannick Menager <4215831-ymenager@users.noreply.gitlab.com>
2019-09-20 09:57:45 -0700

    Added gitlab CI job token to maven

--- prepare-build.sh
+++ prepare-build.sh
@@ -47,13 +47,13 @@ if [[ -n "${MAVEN_REPO_ID}" ]]; then
     echo >>~/.m2/settings.xml "</settings>"
 fi
 
-if [[ -n "${GITLAB_CI_TOKEN}" ]]; then
+if [[ -n "${GITLAB_MAVEN_JOBTOKEN}" ]]; then
     echo  "Adding gitlab maven repository CI job token"
     mkdir -p ~/.m2/
     echo >~/.m2/settings.xml "<settings xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd\" xmlns=\"http://maven.apache.org/SETTINGS/1.1.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
     echo >>~/.m2/settings.xml "<servers>"
     echo >>~/.m2/settings.xml "<server>"
-    echo >>~/.m2/settings.xml "<id>${GITLAB_CI_TOKEN}</id>"
+    echo >>~/.m2/settings.xml "<id>${GITLAB_MAVEN_JOBTOKEN}</id>"
     echo >>~/.m2/settings.xml "<configuration>"
     echo >>~/.m2/settings.xml "<httpHeaders>"
     echo >>~/.m2/settings.xml "<property>"
